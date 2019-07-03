dac26f3a7a6da871dc3b38660b380b9ea5919d3e
GGBBB2000 <ggbbb@mineo.jp>
2019-07-03 13:37:02 +0900

    search(php):Fixed the SQL query
    SQLのクエリが間違っていたので修正

--- search/search_result.php
+++ search/search_result.php
@@ -1,31 +1,42 @@
+<html>
+<head>
+<title>住所検索結果</title>
+</head>
+<body>
 <?php
     $post_data = $_POST["input"];
-    $mysqli = mysqli_connect("localhost", "DBroot", "shizudai", "CSexp1DB");
-    if ($mysqli->connect_errno) {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
-        exit();
-    }
-    $query = "SELECT * FROM zipShizuoka WHERE "
-        . "zip      LIKE '" . $post_data . "%'"
-        . "OR kana1 LIKE '" . $post_data . "%'"
-        . "OR kana2 LIKE '" . $post_data . "%'"
-        . "OR kana3 LIKE '" . $post_data . "%'"
-        . "OR addr1 LIKE '" . $post_data . "%'"
-        . "OR addr2 LIKE '" . $post_data . "%'"
-        . "OR addr3 LIKE '" . $post_data . "%'";
-    sprintf($query, $input);
+    echo "<p> [$post_data] の検索結果</p>";
+    searchAddr($post_data);
 
-    if ($result = mysqli_query($mysqli, $query)) {
-        while($arr = $result->fetch_array(MYSQLI_NUM)) {
-            foreach($arr as $val) {
-                echo "$val ";
-            }
-            echo "<br>";
+    function searchAddr($post_data) {
+        $mysqli = mysqli_connect("localhost", "USER", "PASSWORD", "CSexp1DB");
+        if ($mysqli->connect_errno) {
+            printf("Connect failed: %s\n", $mysqli->connect_error);
+            exit();
         }
+        $query = "SELECT zip, addr1, addr2, addr3 FROM zipAll WHERE "
+            . "zip      LIKE '"  . $post_data . "%'"
+            . "OR kana1 LIKE '%" . $post_data . "%'"
+            . "OR kana2 LIKE '%" . $post_data . "%'"
+            . "OR kana3 LIKE '%" . $post_data . "%'"
+            . "OR addr1 LIKE '%" . $post_data . "%'"
+            . "OR addr2 LIKE '%" . $post_data . "%'"
+            . "OR addr3 LIKE '%" . $post_data . "%'";
 
+        if ($result = mysqli_query($mysqli, $query)) {
+            while($arr = $result->fetch_array(MYSQLI_ASSOC)) {
+                foreach($arr as $val) {
+                    if (strcmp($val,"以下に掲載がない場合") == 0) {
+                        break;
+                    }
+                    echo "$val ";
+                }
+                echo "<br>";
+            }
+        }
+        $result->close();
+        $mysqli->close();
     }
-
-    $result->close();
-    $mysqli->close();
 ?>
-
+</body>
+</html>
