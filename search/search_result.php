6789411e1821c903ceb23a73e2613731f6fc1d85
GGBBB2000 <ggbbb@mineo.jp>
2019-07-03 16:55:16 +0900

    search(php): Bootstrap suport
    Bootstrapへ対応するコードを追加.
    さらに検索結果の総数の表示や元のコードの関数への分割も行った

--- search/search_result.php
+++ search/search_result.php
@@ -1,19 +1,36 @@
-<html>
+<!DOCTYPE HTML>
+<html lang=ja>
 <head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <title>住所検索結果</title>
 </head>
 <body>
 <?php
+    $mysqli = mysqli_connect("localhost", "cs17075", "Nsmhrmlo2", "CSexp1DB");
     $post_data = $_POST["input"];
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
     echo "<p> [$post_data] の検索結果</p>";
-    searchAddr($post_data);
 
-    function searchAddr($post_data) {
-        $mysqli = mysqli_connect("localhost", "USER", "PASSWORD", "CSexp1DB");
-        if ($mysqli->connect_errno) {
-            printf("Connect failed: %s\n", $mysqli->connect_error);
-            exit();
-        }
+    $result = searchAddr($mysqli, $post_data);
+    if ($result->num_rows == 0) {
+        echo "<p> 該当する住所がありませんでした</p>";
+    } else {
+        echo "<p>[" . $result->num_rows . "]件のヒット</p>";
+        printResult($result);
+    }
+
+    $result->close();
+    $mysqli->close();
+
+    function searchAddr($mysqli ,$post_data) {
         $query = "SELECT zip, addr1, addr2, addr3 FROM zipAll WHERE "
             . "zip      LIKE '"  . $post_data . "%'"
             . "OR kana1 LIKE '%" . $post_data . "%'"
@@ -23,19 +40,34 @@
             . "OR addr2 LIKE '%" . $post_data . "%'"
             . "OR addr3 LIKE '%" . $post_data . "%'";
 
-        if ($result = mysqli_query($mysqli, $query)) {
-            while($arr = $result->fetch_array(MYSQLI_ASSOC)) {
-                foreach($arr as $val) {
-                    if (strcmp($val,"以下に掲載がない場合") == 0) {
-                        break;
-                    }
-                    echo "$val ";
-                }
-                echo "<br>";
+        return mysqli_query($mysqli, $query);
+    }
+
+    function printResult($result) {
+        echo '<table class="table table-bordered">'
+            .'<thread>'
+            .'<th>郵便番号</th>'
+            .'<th>住所</th>'
+            .'</thread>'
+            .'<tbody>';
+        while($arr = $result->fetch_array(MYSQLI_ASSOC)) {
+            $zip = $arr["zip"];
+            if (strcmp($arr["addr3"], "以下に掲載がない場合") == 0) {
+                $address = $arr["addr1"] . $arr["addr2"];
+            } else {
+                $address = $arr["addr1"] . $arr["addr2"] . $arr["addr3"];
             }
+            echo "<tr>"
+                    ."<td>"
+                        . $zip
+                    ."</td>"
+                    ."<td>"
+                        . $address
+                    ."</td>"
+                ."</tr>";
         }
-        $result->close();
-        $mysqli->close();
+        echo '</tbody>'
+            .'</table>';
     }
 ?>
 </body>
