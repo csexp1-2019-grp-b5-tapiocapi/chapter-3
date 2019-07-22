e75e9f5654276d989af4d8ddcdc935cd5334453e
Aring8510 <nakamu8510@gmail.com>
2019-07-23 08:13:19 +0900

    add get_tag() in lastA_search.cpp

--- CSexp1-Last/lastA_search.cpp
+++ CSexp1-Last/lastA_search.cpp
@@ -20,7 +20,9 @@ struct info{
 };
 
 
-void get_picture_info(unordered_map<unsigned long long, info*>& mp);
+void get_info(unordered_map<unsigned long long, info*>& mp);
+void get_tag(unordered_map<string, vector<unsigned long long>>& tagmap);
+void print_tagmap(string key, unordered_map<string, vector<unsigned long long>> tagmap);
 
 //void print_result(unordered_map& mp);
 
@@ -32,17 +34,17 @@ int main (int argc, char *argv[]){
 //8376611070,sketch
 
     unordered_map<unsigned long long, info*> mp;
-    get_picture_info(mp);
-    //cout << "print" << endl;
-    //print_result(results);
-    unsigned long long d = 8376611070;
-    printf("key = %llu i->time = %s i->latitude = %lf i->longitude = %lf i->src = %s\n", d, mp[d]->time, mp[d]->latitude, mp[d]->longitude, mp[d]->src);
+    unordered_map<string, vector<unsigned long long>> tagmap;
+    // get_info(mp);
+    // unsigned long long d = 8376611070;
+    // printf("key = %llu i->time = %s i->latitude = %lf i->longitude = %lf i->src = %s\n", d, mp[d]->time, mp[d]->latitude, mp[d]->longitude, mp[d]->src);
+    get_tag(tagmap);
 
     return 0;
 }
 
 
-void get_picture_info(unordered_map<unsigned long long, info*>& mp){
+void get_info(unordered_map<unsigned long long, info*>& mp){
 //8376611070,"2013-01-11 19:49:43",53.656745,-2.083711,http://farm9.static.flickr.com/8050/8376611070_aeb13ec0fe.jpg
     ifstream ifs("geotag_a.csv");
     // ifstream ifs("geotag_test.csv");
@@ -75,15 +77,54 @@ void get_picture_info(unordered_map<unsigned long long, info*>& mp){
         getline(ss, buf, ',');
         buf.erase(buf.begin(), buf.begin() + 11); // http://farmまで消す
         buf.erase(buf.begin() + 1, buf.begin() + 20); // farmX以下.static.flickr.com/まで消す
-        cout << key << endl;
-        buf.erase(buf.end()-4, buf.end()); // .jpgを消す
+        // cout << key << endl;
+        // ↓けつの.jpg消そうとするとメモリリークしちゃう 
+        // buf.erase(buf.end()-4, buf.end()); // .jpgを消す
         i->src = (char *)malloc(buf.size() * sizeof(char));
         strcpy(i->src, buf.c_str());
         mp[key] = i;
-      //   printf("key = %lu i->time = %s i->latitude = %lf i->longitude = %lf i->src = %s\n", key, i->time, i->latitude, i->longitude, i->src);
+        // printf("key = %lu i->time = %s i->latitude = %lf i->longitude = %lf i->src = %s\n", key, i->time, i->latitude, i->longitude, i->src);
         
+        /*
         if (count %10000 == 0){
             cout << count++ << endl;
         }else {count+=1;}
+        */
+    }
+}
+void get_tag(unordered_map<string, vector<unsigned long long>>& tagmap){ 
+    //8376611070,"2013-01-11 19:49:43",53.656745,-2.083711,http://farm9.static.flickr.com/8050/8376611070_aeb13ec0fe.jpg
+    ifstream ifs("tag_a.csv");
+    // ifstream ifs("tag_test.csv");
+    if(ifs.fail()){
+        cerr << "fail" << endl;
+        return;
+    }
+    string line, buf;
+    unsigned long long id;
+    int count =0;
+    while(getline(ifs,line)){
+        stringstream ss(line);
+        getline(ss, buf, ',');
+        id = stoull(buf);
+        getline(ss, buf, ',');
+        if(tagmap.find(buf) == tagmap.end()){ // map空ならベクターを代入しているが必要なのか?
+            vector<unsigned long long> v;
+            tagmap[buf] = v;
+        }
+        tagmap[buf].push_back(id);
+        /*
+        */
+        if (count %100000 == 0){
+            cout << count++ << endl;
+        }else {count+=1;}
+    }
+    // string test = string("car");
+    // print_tagmap(test, tagmap);
+}
+void print_tagmap(string key, unordered_map<string, vector<unsigned long long>> tagmap){
+    cout << key << "\n";
+    for(auto i : tagmap[key]){
+        cout << i << "\n";
     }
 }
