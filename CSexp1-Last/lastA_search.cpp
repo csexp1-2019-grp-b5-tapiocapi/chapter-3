85a81778f9b56e0661668d4054424b6d9ff7bd0e
Aring8510 <nakamu8510@gmail.com>
2019-07-23 08:17:55 +0900

    erase debug print in lastA_search.cpp

--- CSexp1-Last/lastA_search.cpp
+++ CSexp1-Last/lastA_search.cpp
@@ -19,23 +19,19 @@ struct info{
     char *src;
 };
 
-
 void get_info(unordered_map<unsigned long long, info*>& mp);
 void get_tag(unordered_map<string, vector<unsigned long long>>& tagmap);
 void print_tagmap(string key, unordered_map<string, vector<unsigned long long>> tagmap);
 
-//void print_result(unordered_map& mp);
-
 int main (int argc, char *argv[]){
 
-//geotag.csvの例
-//8376611070,"2013-01-11 19:49:43",53.656745,-2.083711,http://farm9.static.flickr.com/8050/8376611070_aeb13ec0fe.jpg
-//tag.csvの例
-//8376611070,sketch
-
+    //geotag.csvの例
+    //8376611070,"2013-01-11 19:49:43",53.656745,-2.083711,http://farm9.static.flickr.com/8050/8376611070_aeb13ec0fe.jpg
+    //tag.csvの例
+    //8376611070,sketch
     unordered_map<unsigned long long, info*> mp;
     unordered_map<string, vector<unsigned long long>> tagmap;
-    // get_info(mp);
+    get_info(mp);
     // unsigned long long d = 8376611070;
     // printf("key = %llu i->time = %s i->latitude = %lf i->longitude = %lf i->src = %s\n", d, mp[d]->time, mp[d]->latitude, mp[d]->longitude, mp[d]->src);
     get_tag(tagmap);
@@ -45,7 +41,6 @@ int main (int argc, char *argv[]){
 
 
 void get_info(unordered_map<unsigned long long, info*>& mp){
-//8376611070,"2013-01-11 19:49:43",53.656745,-2.083711,http://farm9.static.flickr.com/8050/8376611070_aeb13ec0fe.jpg
     ifstream ifs("geotag_a.csv");
     // ifstream ifs("geotag_test.csv");
     if(ifs.fail()){
@@ -56,7 +51,6 @@ void get_info(unordered_map<unsigned long long, info*>& mp){
     string line, buf;
     unsigned long key;
     double latitude,longitude; 
-    int count = 0;
     // tm t = {};
     while(getline(ifs, line)){
         stringstream ss(line);
@@ -77,23 +71,16 @@ void get_info(unordered_map<unsigned long long, info*>& mp){
         getline(ss, buf, ',');
         buf.erase(buf.begin(), buf.begin() + 11); // http://farmまで消す
         buf.erase(buf.begin() + 1, buf.begin() + 20); // farmX以下.static.flickr.com/まで消す
-        // cout << key << endl;
         // ↓けつの.jpg消そうとするとメモリリークしちゃう 
         // buf.erase(buf.end()-4, buf.end()); // .jpgを消す
         i->src = (char *)malloc(buf.size() * sizeof(char));
         strcpy(i->src, buf.c_str());
         mp[key] = i;
-        // printf("key = %lu i->time = %s i->latitude = %lf i->longitude = %lf i->src = %s\n", key, i->time, i->latitude, i->longitude, i->src);
-        
-        /*
-        if (count %10000 == 0){
-            cout << count++ << endl;
-        }else {count+=1;}
-        */
     }
 }
 void get_tag(unordered_map<string, vector<unsigned long long>>& tagmap){ 
-    //8376611070,"2013-01-11 19:49:43",53.656745,-2.083711,http://farm9.static.flickr.com/8050/8376611070_aeb13ec0fe.jpg
+    //tag.csvの例
+    //8376611070,sketch
     ifstream ifs("tag_a.csv");
     // ifstream ifs("tag_test.csv");
     if(ifs.fail()){
@@ -102,7 +89,6 @@ void get_tag(unordered_map<string, vector<unsigned long long>>& tagmap){
     }
     string line, buf;
     unsigned long long id;
-    int count =0;
     while(getline(ifs,line)){
         stringstream ss(line);
         getline(ss, buf, ',');
@@ -113,11 +99,6 @@ void get_tag(unordered_map<string, vector<unsigned long long>>& tagmap){
             tagmap[buf] = v;
         }
         tagmap[buf].push_back(id);
-        /*
-        */
-        if (count %100000 == 0){
-            cout << count++ << endl;
-        }else {count+=1;}
     }
     // string test = string("car");
     // print_tagmap(test, tagmap);
